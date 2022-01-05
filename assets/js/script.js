e03d96377873767e47fd93e55661e13c430794da
coyote <megan.metelak@gmail.com>
2022-01-05 11:57:08 -0600

    adjusted html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
+var searchFormEl = document.querySelector("#search-form");
+var nameInputEl = document.querySelector("#text-field");
 // add text to search box
 
 // add click function to search button 
 
 // connect search button to text entered
+
+// add city searched to search history list
+
