5f85f3f45989bb251e62337ce5a9fe9ab7c55659
coyote <megan.metelak@gmail.com>
2022-01-05 13:00:38 -0600

    form submit handler/listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 var searchFormEl = document.querySelector("#search-form");
-var nameInputEl = document.querySelector("#text-field");
-// add text to search box
+var textFieldEl = document.querySelector("#text-field");
+
+// add text to search box and connect search button to text entered
+var formSubmitHandler = function(event){
+
+}
 
 // add click function to search button 
-
-// connect search button to text entered
+searchFormEl.addEventListener("submit", formSubmitHandler);
 
 // add city searched to search history list
 
